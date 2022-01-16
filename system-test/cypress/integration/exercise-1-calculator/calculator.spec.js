beb30d581f18cc242fc5feed0cac194224c7c29b
Kunapot Pairat (อ๊อฟ) <kunapot@thinknet.co.th>
2022-01-17 00:49:59 +0700

    Comment code for exercise 1.

--- system-test/cypress/integration/exercise-1-calculator/calculator.spec.js
+++ system-test/cypress/integration/exercise-1-calculator/calculator.spec.js
@@ -17,13 +17,13 @@ const Calculator = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
-  divide: (a, b) => {
-    if (b === 0) {
-      throw new Error('Cannot divide by zero.')
-    }
+  // divide: (a, b) => {
+  //   if (b === 0) {
+  //     throw new Error('Cannot divide by zero.')
+  //   }
 
-    return a / b
-  }
+  //   return a / b
+  // }
 }
 
 describe('Calculator operations', () => {
@@ -56,45 +56,45 @@ describe('Calculator operations', () => {
   })
 
   // 1. Write test for Calculator.multiply
-  describe('Multiply', () => {
-    it('Multiply 4 by 3 should return 12', () => {
-      const result = Calculator.multiply(4, 3)
+  // describe('Multiply', () => {
+  //   it('Multiply 4 by 3 should return 12', () => {
+  //     const result = Calculator.multiply(4, 3)
   
-      expect(result).to.equal(12)
-    })
+  //     expect(result).to.equal(12)
+  //   })
 
-    it('Multiply 1 by 0 should return 0', () => {
-      const result = Calculator.multiply(1, 0)
+  //   it('Multiply 1 by 0 should return 0', () => {
+  //     const result = Calculator.multiply(1, 0)
   
-      expect(result).to.equal(0)
-    })
-  })
+  //     expect(result).to.equal(0)
+  //   })
+  // })
   
 
   // 2. Write test for Calculator.divide
   // 2.1 Use TDD style
-  describe('Divide', () => {
-    it('Divide 6 by 3 should return 2', () => {
-      const result = Calculator.divide(6, 3)
+  // describe('Divide', () => {
+  //   it('Divide 6 by 3 should return 2', () => {
+  //     const result = Calculator.divide(6, 3)
 
-      expect(result).to.equal(2)
-    })
+  //     expect(result).to.equal(2)
+  //   })
 
-    it('Divide 0 by 1 should return 0', () => {
-      const result = Calculator.divide(0, 1)
+  //   it('Divide 0 by 1 should return 0', () => {
+  //     const result = Calculator.divide(0, 1)
 
-      expect(result).to.equal(0)
-    })
+  //     expect(result).to.equal(0)
+  //   })
 
-    it('Divide 3 by 2 should return 1.5', () => {
-      const result = Calculator.divide(3, 2)
+  //   it('Divide 3 by 2 should return 1.5', () => {
+  //     const result = Calculator.divide(3, 2)
 
-      expect(result).to.equal(1.5)
-    })
+  //     expect(result).to.equal(1.5)
+  //   })
 
-    // 2.2 Handle divide by zero
-    it('Should throw error when divide by zero', () => {
-      expect(() => Calculator.divide(12, 0)).to.throw()
-    })
-  })
+  //   // 2.2 Handle divide by zero
+  //   it('Should throw error when divide by zero', () => {
+  //     expect(() => Calculator.divide(12, 0)).to.throw()
+  //   })
+  // })
 })
